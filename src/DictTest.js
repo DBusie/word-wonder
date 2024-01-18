ad87034d323cbe27153cc73215a6b77ecb712564
DBusie <132249012+DBusie@users.noreply.github.com>
2024-01-18 21:48:12 +0200

    api integration

--- src/DictTest.js
+++ src/DictTest.js
@@ -1,18 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
-export default function DictTest(response) {
-  //const [data, setData] = useState("");
-  //const [searchWord, setSearchWord] = useState("");
-  //const word = "beautiful";
-  const apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/beautiful`;
-  axios
-    .get(apiUrl)
-    .then(function (response) {
-      console.log(response.data);
-      return response.data.meanings;
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+export default function DictTest() {
+  const [data, setData] = useState("");
+  const [searchWord, setSearchWord] = useState("");
+
+  function getMeaning() {
+    const apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${searchWord}`;
+    axios
+      .get(apiUrl)
+      .then(function (response) {
+        setData(response.data[0]);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  return (
+    <div className="WordFinder">
+      <div className="searchBox">
+        <input
+          type="text"
+          placeholder="Search..."
+          onChange={(e) => {
+            setSearchWord(e.target.value);
+          }}
+        />
+        <button onClick={getMeaning}>Search</button>
+      </div>
+      <div>
+        <p>{data}</p>
+      </div>
+    </div>
+  );
 }
